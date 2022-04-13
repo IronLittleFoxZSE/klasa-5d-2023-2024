1b97e4396f48b914f1e25648bd572f62c3ccab79
Łukasz Felisek <felisek1@wp.pl>
2022-04-13 10:51:20 +0200

    koniec lekcji

---
+++ Pierwszy projekt/ProjektUczniowie/Baza danych/Model/Klasa.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjektUczniowie.Baza_danych.Model
+{
+    class Klasa
+    {
+        public TYPE Type { get; set; }
+
+    }
+}
