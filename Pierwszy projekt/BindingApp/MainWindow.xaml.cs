160856cb9df448ef5f90050d07fefea6181a5d45
Łukasz Felisek <lfelisek@e-zsel.kielce.eu>
2022-10-03 12:39:00 +0200

    jn

--- Pierwszy projekt/BindingApp/MainWindow.xaml.cs
+++ Pierwszy projekt/BindingApp/MainWindow.xaml.cs
@@ -12,20 +12,34 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.ComponentModel;
 
 namespace BindingApp
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : Window, INotifyPropertyChanged
     {
         public string Message { get; set; }
+
+        public string RandomNumber { get; set; }
 
         public MainWindow()
         {
             InitializeComponent();
         }
+
+        #region INotifyPropertyChanged
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        #endregion
 
         private void textBoxValue_TextChanged(object sender, TextChangedEventArgs e)
         {
@@ -36,6 +50,13 @@ namespace BindingApp
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             MessageBox.Show(Message);
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            int randomNumber = new Random().Next();
+            RandomNumber = randomNumber.ToString();
+            OnPropertyChanged("RandomNumber");
         }
     }
 }
