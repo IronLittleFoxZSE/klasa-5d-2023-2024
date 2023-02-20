6f49f6b55daf5967064c6ee175ea5b663e7e7d72
IronLittleFox <lukasz.felisek@gmail.com>
2023-02-20 12:37:27 +0100

    koniec lekcji

---
+++ Pierwszy projekt/ListOfContactsApp/ListOfContactsApp/MainViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+using Xamarin.Essentials;
+
+namespace ListOfContactsApp
+{
+    class MainViewModel
+    {
+        public ObservableCollection<Contact> CollectionOfContacts { get; set; }
+
+        public MainViewModel()
+        {
+            try
+            {
+                //Contacts.PickContactAsync();
+                CollectionOfContacts = new ObservableCollection<Contact>();
+                var contacts = Contacts.GetAllAsync().Result;
+                if (contacts == null)
+                    return;
+                foreach (var item in contacts)
+                {
+                    CollectionOfContacts.Add(item);
+                }
+            }
+            catch (Exception)
+            {
+            }
+        }
+    }
+}
