0a1b3d964402d052aa4a74e0b2df537d3ebf0472
IronLittleFox <lukasz.felisek@gmail.com>
2023-02-21 10:41:12 +0100

    koniec lekcji

--- Pierwszy projekt/ListOfContactsApp/ListOfContactsApp/MainPage.xaml.cs
+++ Pierwszy projekt/ListOfContactsApp/ListOfContactsApp/MainPage.xaml.cs
@@ -13,7 +13,6 @@ namespace ListOfContactsApp
         public MainPage()
         {
             InitializeComponent();
-
         }
     }
 }
--- Pierwszy projekt/ListOfContactsApp/ListOfContactsApp/MainViewModel.cs
+++ Pierwszy projekt/ListOfContactsApp/ListOfContactsApp/MainViewModel.cs
@@ -2,13 +2,31 @@
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Text;
+using System.Windows.Input;
 using Xamarin.Essentials;
+using Xamarin.Forms;
 
 namespace ListOfContactsApp
 {
     class MainViewModel
     {
         public ObservableCollection<Contact> CollectionOfContacts { get; set; }
+
+        private ICommand phoneCallCommand;
+        public ICommand PhoneCallCommand
+        {
+            get
+            {
+                if (phoneCallCommand == null)
+                    phoneCallCommand = new Command<Contact>(
+                        contact =>
+                        {
+
+                        }
+                        );
+                return phoneCallCommand;
+            }
+        }
 
         public MainViewModel()
         {
